dbbb8101fde58128929b2098a7d3abdb46ea8ad8
emuange <emuange@gmail.com>
2024-01-19 14:07:33 +0300

    Completed Edit and Delete tasks.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { Header } from "./components/Header";
 import { AddTask } from "./components/AddTask";
@@ -7,13 +7,24 @@ import { ShowTask } from "./components/ShowTask";
 import './App.css';
 
 function App() {
-  const [tasklist, setTasklist] = useState([]);
+  const [tasklist, setTasklist] = useState(JSON.parse(localStorage.getItem("tasklist")) || []);
+  const [task, setTask] = useState({});
+
+  useEffect(() => {
+    localStorage.setItem("tasklist", JSON.stringify(tasklist));
+  }, [tasklist]);
 
   return (
     <div className="App">
       <Header />
-      <AddTask tasklist={tasklist} setTasklist={setTasklist} />
-      <ShowTask tasklist={tasklist} setTasklist={setTasklist} />
+      <AddTask 
+      tasklist={tasklist} setTasklist={setTasklist} 
+      task={task} setTask={setTask}
+      />
+      <ShowTask 
+      tasklist={tasklist} setTasklist={setTasklist} 
+      task={task} setTask={setTask}
+      />
     </div>
   );
 }
--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,24 +1,34 @@
 
-export const AddTask = ({tasklist, setTasklist}) => {
+export const AddTask = ({tasklist, setTasklist, task, setTask}) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    const date = new Date();
-    const newTask = {
-       id: date.getTime(), 
-       name: e.target.task.value, 
-       time:`${date.toLocaleTimeString()} ${date.toLocaleDateString()}`
-      }
-      e.target.task.value = "";
-    setTasklist([...tasklist, newTask]);
+    if(task.id)
+    {
+      const date = new Date();
+      const updatedTasklist = tasklist.map((todo) => (
+        todo.id === task.id ? {id: task.id, name: task.name, time:`${date.toLocaleTimeString()} ${date.toLocaleDateString()}`} : todo));
+
+        setTasklist(updatedTasklist);
+        setTask({});
+    }else{
+      const date = new Date();
+      const newTask = {
+         id: date.getTime(), 
+         name: e.target.task.value, 
+         time:`${date.toLocaleTimeString()} ${date.toLocaleDateString()}`
+        }
+        setTasklist([...tasklist, newTask]);
+        setTask({});
+    }    
   }
 
   return (
     <section className='addTask'>
       <form onSubmit={handleSubmit}>
-        <input type="text" name='task' placeholder='add task' autoComplete='off' maxLength={25}/>
-        <button type="submit">Add</button>
+        <input type="text" name='task' value={task.name || ""} placeholder='add task' autoComplete='off' maxLength="25" onChange={e => setTask({...task, name: e.target.value})}/>
+        <button type="submit">{ task.id ? "Update" : "Add"}</button>
       </form>
     </section>
   )
